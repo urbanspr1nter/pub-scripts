d812199df38b12601fd0b81af3f8df17a85881ad
Roger Ngo <urbanspr1nter@gmail.com>
2024-07-10 11:54:58 -0700

    Adding udev rules

--- scripts/pipewire-restart.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-systemctl --user restart pipewire.service
-systemctl --user restart pipewire-pulse.service
-systemctl --user status pipewire.service
-systemctl --user status pipewire-pulse.service
