68e7225fba5a11238c7f8f07c5a433b2f5651009
Roger Ngo <urbanspr1nter@gmail.com>
2024-07-09 18:19:39 -0700

    Base installation script

---
+++ base/install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+mkdir -p $HOME/code
+
+# install the most essential things
+sudo apt install curl vim build-essential git python3-pip gnome-tweaks -y
+
+# always need flatpak
+sudo apt install flatpak -y
+sudo apt install gnome-software-plugin-flatpak -y
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+# neovim!
+sudo apt install software-properties-common -y
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt update
+sudo apt install neovim -y
+
+mkdir -p $HOME/.config/nvim
+
+# clone my neovim config
+git clone https://github.com/urbanspr1nter/kickstart.nvim.git $HOME/.config/nvim
+
+
