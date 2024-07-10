e7337a9587d4fc726ede7e65d10b968d035371cb
Roger Ngo <urbanspr1nter@gmail.com>
2024-07-09 18:18:45 -0700

    Adding pipewire restart script

--- scripts/pipewire-restart.sh
+++ scripts/pipewire-restart.sh
@@ -2,4 +2,5 @@
 
 systemctl --user restart pipewire.service
 systemctl --user restart pipewire-pulse.service
-
+systemctl --user status pipewire.service
+systemctl --user status pipewire-pulse.service
