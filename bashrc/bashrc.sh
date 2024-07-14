5ee0de4232ca7857ef2e6daf64d411202f99b7e4
Roger Ngo <urbanspr1nter@gmail.com>
2024-07-14 04:46:42 -0700

    Don't use ifconfig anymore

--- bashrc/bashrc.sh
+++ bashrc/bashrc.sh
@@ -5,3 +5,6 @@ export EDITOR=vim
 # 109 is the keyboard code for PageDown 
 alias lg="/path/to/looking-glass-client -m 109"
 alias auto-cpufreq-conf="sudo vim /etc/auto-cpufreq.conf"
+
+# Don't use ifconfig anymore
+alias ifconfig="echo 'You should use `ip addr` instead to find the IP address.'"
