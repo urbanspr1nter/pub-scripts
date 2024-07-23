ec388bd7037f44460b302be8785bf91384d37f6e
Roger Ngo <rngo@netflix.com>
2024-07-22 21:12:55 -0700

    cpu-autofreq

--- auto-cpufreq/configs/power-lp.sh
+++ auto-cpufreq/configs/power-lp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo cat $XDG_CONFIG_HOME/auto-cpufreq/w9-3475x-lp.conf | sudo tee /etc/auto-cpufreq.conf &> /dev/null
+sudo cat /home/rngo/.config/auto-cpufreq/w9-3475x-lp.conf | sudo tee /etc/auto-cpufreq.conf &> /dev/null
 sudo systemctl restart auto-cpufreq
 
 echo "*** Low-Power Profile Installed! ***"
--- auto-cpufreq/configs/power-lpp.sh
+++ auto-cpufreq/configs/power-lpp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo cat $XDG_CONFIG_HOME/auto-cpufreq/w9-3475x-lpp.conf  | sudo tee /etc/auto-cpufreq.conf &> /dev/null
+sudo cat /home/rngo/.config/auto-cpufreq/w9-3475x-lpp.conf  | sudo tee /etc/auto-cpufreq.conf &> /dev/null
 sudo systemctl restart auto-cpufreq
 
 echo "*** VeryLow-Power Profile Installed! ***"
