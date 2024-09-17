917545e938079d1883bccfa4ed8907e8047c40cb
Roger Ngo <urbanspr1nter@gmail.com>
2024-09-16 17:37:09 -0700

    Sway screenshot

--- base/sway.sh
+++ base/sway.sh
@@ -6,6 +6,9 @@ sudo echo "Elevating privileges..."
 
 sudo apt install brightnessctl light sway swaybg swayidle swayimg swaylock waybar wofi fonts-font-awesome -y
 
+# For screenshotting
+sudo apt install jq grim slurp wl-clipboard imagemagick
+
 mkdir -p $HOME/.config/sway $HOME/.config/waybar $HOME/.config/wofi
 
 # copy sway config
@@ -21,3 +24,7 @@ cp -r $PWD/config/wofi/* $HOME/.config/wofi
 mkdir -p $HOME/Pictures/Wallpaper
 cp $PWD/wallpapers/catppuccin.jpg $HOME/Pictures/Wallpaper
 
+# setup sway-screenshot
+mkdir -p $HOME/.local/bin
+git clone https://github.com/Gustash/sway-screenshot.git $HOME/.local/bin
+echo "export PATH=\"$PATH:/home/rngo/.local/bin/sway-screenshot\" >> $HOME/.bashrc
