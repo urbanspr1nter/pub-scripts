1bdbb5cfc2251d7f197edd594a888c8248dc325b
Roger Ngo <urbanspr1nter@gmail.com>
2024-11-10 11:01:39 -0800

    Update

--- base/i3.sh
+++ base/i3.sh
@@ -7,7 +7,7 @@ sudo apt install clang cmake cmake-data pkg-config python3-sphinx python3-packag
     libuv1-dev libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev \
     python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev \
     libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev \
-    libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev
+    libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev -y
 
 mkdir -p $HOME/.config/i3
 mkdir -p $HOME/.config/i3status
